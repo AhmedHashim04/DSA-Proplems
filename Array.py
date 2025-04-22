2b4bd889804e890a1c054e01eccda9622c8c85a7
ahmedhashim04 <ahmedha4im7@gmail.com>
2025-04-22 15:32:50 +0200

    Insert By Shifting

--- Array.py
+++ Array.py
@@ -10,95 +10,82 @@ Array Properties in C Language :
     -no append no len no check Bounds
     -if you want to append you need to create new array and copy old elements
 """ 
+
 class Array:
+    def __init__(self, size):
+        self.size = size                    # user size
+        self._capacity = max(16, 2*size)    # actual memory size
 
-    def __init__(self,size):
-        self.expand_times = 0
-        self.size = size
-        self._capacity = 16 # 16 is actual size of memory
-
-        array  = ctypes.py_object * self._capacity
-        self.memory = array()
+        array_data_type = ctypes.py_object * self._capacity
+        self.memory = array_data_type()
 
         for i in range(self._capacity):
             self.memory[i] = None
 
+    def expand_capacity(self):
+        # Double the actual array size
+        self._capacity *= 2
+        print(f'Expand capacity to {self._capacity}')
 
+        # create a new array of _capacity
+        array_data_type = ctypes.py_object * self._capacity
+        new_memory = array_data_type()
+
+        for i in range(self.size):  # copy
+            new_memory[i] = self.memory[i]
+
+        # use the new memory and delete old one
+        del self.memory
+        self.memory = new_memory
+
+    def append(self, value):
+        if self.size == self._capacity:
+            self.expand_capacity()
+        self.memory[self.size] = value
+        self.size += 1
 
     def __len__(self):
         return self.size
-    
-    def __getitem__(self,index):
-        return self.memory[index]
 
-    def __setitem__(self,index,value):
-        self.memory[index] = value
-    
+    def __getitem__(self, idx):
+        return self.memory[idx]  # Is valid idx?
+
+    def __setitem__(self, idx, value):
+        self.memory[idx] = value
+
     def __repr__(self):
-        result = 'arr() {'
+        result = ''
         for i in range(self.size):
             result += str(self.memory[i]) + ', '
-        
-        result = result[:len(result)-2]
-        result += '}'
         return result
 
-    def append(self,value,capacity_trick=0):
-        # Start Capacity trick
-        if capacity_trick :
-            if self.size == self._capacity:
-                print(f"Capacity now is : {self._capacity}")
-                self._capacity *= 2
-                self.expand_times += 1
-                print(f'Expand capacity to {self._capacity}')
+    def insert(self,idx,value):
 
-                #Create new array
-                new_memory = (ctypes.py_object * self._capacity)()
+        if self.size == self._capacity:
+            # we can't add any more
+            self.expand_capacity()
 
-                # Copy elements
-                for i in range(self.size):
-                    new_memory[i] = self.memory[i]
-                
-                self.memory = new_memory
-
-        else:
-            if self.size == self._capacity:
-                print(f"Capacity now is : {self._capacity}")
-                self._capacity += 1
-                self.expand_times += 1
-                print(f'Expand capacity to {self._capacity}')
-                #Create new array
-                new_memory = (ctypes.py_object * self._capacity)()
-
-                # Copy elements
-                for i in range(self.size):
-                    new_memory[i] = self.memory[i]
-                
-                self.memory = new_memory
-    
-        self.memory[self.size] = value
+        # Shift all the data to right first CORRECTLY
+        # Shift range (idx, size-1) from the back
+        for p in range(self.size - 1, idx - 1, - 1):
+            self.memory[p + 1] = self.memory[p]
+        self.memory[idx] = value
         self.size += 1
-        print(f"Capacity now is : {self._capacity}")
 
+        # Common mistake to iterate from begin to end
+        # the whole array right array will be arr[idx]
 
-
-arr = Array(16)
-
-for i in range(arr.size):
-    arr.memory[i] = i+1
-
-
-
-for i in range(10 ** 4):
-    arr.append(i,1)
-    # arr.append(i)
-
-print(arr.expand_times)
-
-print(len(arr))
-"""
-1/10
-3/100
-6/1000
-
-"""
+array = Array(0)
+array.append(56)
+array.append('hello')
+print(array)
+#56, hello,
+array.insert(0, 'A0')
+print(array)
+# A0, 56, hello,
+array.insert(2, 'A2')
+print(array)
+# A0, 56, A2, hello,
+array.insert(1, -9)
+print(array)
+# A0, -9, 56, A2, hello,
