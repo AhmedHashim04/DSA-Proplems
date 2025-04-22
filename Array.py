c069d625c281c541d33a3cf86aeb32863dfc80b7
Ahmed Hashim <ahmedha4im7@gmail.com>
2025-04-23 00:14:54 +0200

    Implement negative indexing support in insert method

--- Array.py
+++ Array.py
@@ -30,6 +30,9 @@ class Array:
         array_data_type = ctypes.py_object * self._capacity
         new_memory = array_data_type()
 
+        for i in range(self._capacity):  # initialize new memory with None
+            new_memory[i] = None
+
         for i in range(self.size):  # copy
             new_memory[i] = self.memory[i]
 
@@ -57,39 +60,62 @@ class Array:
         for i in range(self.size):
             result += str(self.memory[i]) + ', '
         return result
-
     def insert(self, idx, value):
-        '''
+        """
         ● In the lecture, we implement insert function to allow only integer indices in the
-            range [0, size-1]
-        '''
+        range [0, size-1]
+        ● However, list in Python allows negative indexing
+        ● Change the code to allow negative integer indexing
+            ○ In practice, the user may also make mistakes by passing a float or a wrong data type
+            ○ Handling these mistakes is out of our scope
+        ● Change your code to work according to the list as follows:
+        """
         if self.size == self._capacity:
             self.expand_capacity()
-        for i in range(self.size-self._capacity-1, idx-self._capacity-1, -1):
-            self.memory[i+1] = self.memory[i]
-        self.memory[idx] = value
+        if idx > self.size:
+            self.append(value)
+            return
+        if idx >= 0 :
+            for p in range(self.size - 1, idx - 1, - 1):
+                self.memory[p + 1] = self.memory[p]
+            self.memory[idx] = value
+
+        else :
+            for p in range(self.size-self._capacity-1, (idx+self.size-self._capacity-1), - 1):
+                self.memory[p + 1] = self.memory[p]
+            self.memory[self.size-self._capacity+idx] = value
+
         self.size += 1
+# -3 idx
+# size = 4
+# total -16
 
+def test_insert():
+    array = Array(0)
+    array.append(1)
+    array.append(2)
+    array.append(3)
+    array.append(4)
+    # 1, 2, 3, 4
+    array.insert(-1, -10)
+    print(array)  # 1, 2, 3, -10, 4,
 
-array = Array(0)
-array.append(56)
-array.append('hello')
-print(array)
-#56 ,hello,---
-array.insert(0, 'A0')
-# A0, 56, hello,
-array.insert(2, 'A2')
-print(array)
-# A0, 56, A2, hello,
-array.insert(1, -9)
-print(array)
-# A0, -9, 56, A2, hello, none none none ...
+    array.insert(-2, -20)
+    print(array)  # 1, 2, 3, -20, -10, 4,
 
-'''
-● In the lecture, we implement insert function to allow only integer indices in the
-range [0, size-1]
-● However, list in Python allows negative indexing
-● Change the code to allow negative integer indexing
-    ○ In practice, the user may also make mistakes by passing a float or a wrong data type
-    ○ Handling these mistakes is out of our scope
-● Change your code to work according to the list as follows:'''
+    array.insert(-3, -30)
+    print(array)  # 1, 2, 3, -30, -20, -10, 4,
+
+    array.insert(-4, -40)
+    print(array)  # 1, 2, 3, -40, -30, -20, -10, 4,
+
+    array.insert(-5, -50)
+    print(array)  # 1, 2, 3, -50, -40, -30, -20, -10, 4,
+
+    array.insert(8, 80)
+    print(array)  # 1, 2, 3, -50, -40, -30, -20, -10, 80, 4,
+
+    array.insert(20, 90)
+    print(array)  # 1, 2, 3, -50, -40, -30, -20, -10, 80, 4, 90,
+test_insert()
+
